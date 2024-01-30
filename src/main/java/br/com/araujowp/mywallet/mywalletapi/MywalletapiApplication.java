3f7d6409a98800f904603d6265b027ef130430b3
Wagner Araujo <araujowp@gmail.com>
2024-01-30 07:02:46 -0300

    solve url not find in application.properties

--- src/main/java/br/com/araujowp/mywallet/mywalletapi/MywalletapiApplication.java
+++ src/main/java/br/com/araujowp/mywallet/mywalletapi/MywalletapiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
-public class MywalletapiApplication {
+public class MyWalletapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MywalletapiApplication.class, args);
+		SpringApplication.run(MyWalletapiApplication.class, args);
 	}
 
 }
